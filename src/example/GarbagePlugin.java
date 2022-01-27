a8fe5af98de45c4dea03728c56b56b117bbcf2b4
mse-k <37938606+mse-k@users.noreply.github.com>
2022-01-27 16:24:57 -0500

    Update GarbagePlugin.java

--- src/example/GarbagePlugin.java
+++ src/example/GarbagePlugin.java
@@ -14,16 +14,30 @@ import mindustry.world.blocks.storage.*;
 import mindustry.world.*;
 
 public class GarbagePlugin extends Plugin{
+    private Team HandleTeamArg(string arg){
+        try{
+            int number = Integer.parseInt(arg);
+            team = Team.get(number);
+            return team;
+        }catch (NumberFormatException ex){
+            player.sendMessage("[scarlet]" + arg + " is not a valid team, teams are any valid signed integer." +
+"\n[grey]0 = Derelict\n[yellow]1 = Sharded\n[red]2 = Crux\n[green]3 = Green\n[purple]4 = Purple\n[blue]5 = Blue");
+            return null;
+        }
+    }
+    private Player HandlePlayerArg(string arg){
+        Player other = Groups.player.find(p -> Strings.stripColors(p.name.replace(" ", "")).equalsIgnoreCase(arg));
+        if(other == null){
+            player.sendMessage("[scarlet]Couldnt find the player \"" + arg + "\" (Did he leave?)");
+            return null;
+        }
+    }
     //register commands that player can invoke in-game
     @Override
     public void registerClientCommands(CommandHandler handler){
-        
         handler.<Player>register("msg", "<player> <text...>", "Send a message only to another player.", (args, player) -> {
-            Player other = Groups.player.find(p -> Strings.stripColors(p.name.replace(" ", "")).equalsIgnoreCase(args[0]));
-            if(other == null){
-                player.sendMessage("[scarlet]Couldnt find the player \"" + args[0] + "\" (Did he leave?)");
-                return;
-            }
+            Player other = HandlePlayerArg(args[0]);
+            if (other == null) return;
             player.sendMessage("[red]<[yellow]You[] -> [cyan]" + Strings.stripColors(other.name) + "[red]> [lightgrey]" + args[1]);
             other.sendMessage("[red]<[cyan]" + Strings.stripColors(player.name) + "[red] -> [yellow]You[]> [lightgrey]" + args[1]);
         });
@@ -31,20 +45,11 @@ public class GarbagePlugin extends Plugin{
         handler.<Player>register("team", "<team> [player]", "Sets the team of yourself or another player.", (args, player) -> {
             Player other = player;
             if(args.length == 2){
-                other = Groups.player.find(p -> Strings.stripColors(p.name.replace(" ", "")).equalsIgnoreCase(args[1]));
+                other = HandlePlayerArg(args[1]);
+                if (other == null) return;
             }
-            if(other == null){
-                player.sendMessage("[scarlet]Couldnt find the player \"" + args[1] + "\" (Did he leave?)");
-                return;
-            }
-            Team team = Team.get(0);
-            try{
-                int number = Integer.parseInt(args[0]);
-                team = Team.get(number);
-            }catch (NumberFormatException ex){
-                player.sendMessage("[scarlet]" + args[0] + " is not a valid team, teams are any valid signed integer.\n[grey]0 = Derelict\n[yellow]1 = Sharded\n[red]2 = Crux\n[green]3 = Green\n[purple]4 = Purple\n[blue]5 = Blue");
-                return;
-            }
+            Team team = HandleTeamArg(args[0]);
+            if (team == null) return;
             other.team(team);
             if(other==player){
                 player.sendMessage("[lightgrey]Set your team to team " + args[0]);
@@ -57,19 +62,8 @@ public class GarbagePlugin extends Plugin{
         handler.<Player>register("setteam", "<team> [player]", "Alt of /team for foos client users until its fixed", (args, player) -> {
             Player other = player;
             if(args.length == 2){
-                other = Groups.player.find(p -> Strings.stripColors(p.name.replace(" ", "")).equalsIgnoreCase(args[1]));
-            }
-            if(other == null){
-                player.sendMessage("[scarlet]Couldnt find the player \"" + args[1] + "\" (Did he leave?)");
-                return;
-            }
-            Team team = Team.get(0);
-            try{
-                int number = Integer.parseInt(args[0]);
-                team = Team.get(number);
-            }catch (NumberFormatException ex){
-                player.sendMessage("[scarlet]" + args[0] + " is not a valid team, teams are any valid signed integer.\n[grey]0 = Derelict\n[yellow]1 = Sharded\n[red]2 = Crux\n[green]3 = Green\n[purple]4 = Purple\n[blue]5 = Blue");
-                return;
+                other = HandlePlayerArg(args[1]);
+                if (other == null) return;
             }
             other.team(team);
             if(other==player){
@@ -81,18 +75,11 @@ public class GarbagePlugin extends Plugin{
         });
         
         handler.<Player>register("killall", "[team]", "Kills all units, optionally of just one team.", (args, player) -> {
-            EntityGroup<Unit> unitsToKill = Groups.unit;
             if(args.length == 1) {
-                Team team = Team.get(0);
-                try{
-                    int number = Integer.parseInt(args[0]);
-                    team = Team.get(number);
-                }catch (NumberFormatException ex){
-                    player.sendMessage("[scarlet]" + args[0] + " is not a valid team, teams are any valid signed integer.\n[grey]0 = Derelict\n[yellow]1 = Sharded\n[red]2 = Crux\n[green]3 = Green\n[purple]4 = Purple\n[blue]5 = Blue");
-                    return;
-                }
+                Team team = HandleTeamArg(args[0]);
+                if (team == null) return;
                 Call.sendMessage("[lightgrey]All units on team " + args[0] + " have been killed by " + player.name + "[lightgrey].");
-                for(Unit u:unitsToKill){
+                for(var iter = Groups.unit.iterator(), Unit u; iter.hasNext(); u = iter.next();) {
                     if(u.team == team && !u.spawnedByCore){
                         Call.unitDespawn(u);
                     }
@@ -100,7 +87,7 @@ public class GarbagePlugin extends Plugin{
                 return;
             }
             Call.sendMessage("[lightgrey]All units have been killed by " + player.name + "[lightgrey].");
-            for(Unit u:unitsToKill){
+            for(var iter = Groups.unit.iterator(), Unit u; iter.hasNext(); u = iter.next();) {
                 if(!u.spawnedByCore){
                     Call.unitDespawn(u);
                 }
@@ -108,26 +95,19 @@ public class GarbagePlugin extends Plugin{
         });
         
         handler.<Player>register("wipe", "[team] [cores]", "Removes all buildings, optionally of just one team. Can remove cores of a team too.", (args, player) -> {
-            EntityGroup<Building> buildsToKill = Groups.build;
             if(args.length > 0) {
                 boolean cores = false;
                 if(args.length == 2){
                     cores = args[1].equalsIgnoreCase("y") || args[1].equalsIgnoreCase("yes");
                 }
-                Team team = Team.get(0);
-                try{
-                    int number = Integer.parseInt(args[0]);
-                    team = Team.get(number);
-                }catch (NumberFormatException ex){
-                    player.sendMessage("[scarlet]" + args[0] + " is not a valid team, teams are any valid signed integer.\n[grey]0 = Derelict\n[yellow]1 = Sharded\n[red]2 = Crux\n[green]3 = Green\n[purple]4 = Purple\n[blue]5 = Blue");
-                    return;
-                }
+                Team team = HandleTeamArg(args[0]);
+                if (team == null) return;
                 if(team == Team.get(1) && cores){
-                    player.sendMessage("[scarlet]No.");
+                    player.sendMessage("[scarlet]Thats just /gameover with extra steps...");
                     return;
                 }
                 Call.sendMessage("[lightgrey]All builds on team " + args[0] + " have been wiped by " + player.name + "[lightgrey].");
-                for(Building b:buildsToKill){
+                for(var iter = Groups.build.iterator(), Building b; iter.hasNext(); b = iter.next();) {
                     if(b.team == team && (!(b.block instanceof CoreBlock) || cores)){
                         b.tile.setNet(Blocks.air);
                     }
@@ -135,11 +115,16 @@ public class GarbagePlugin extends Plugin{
                 return;
             }
             Call.sendMessage("[lightgrey]All builds have been wiped by " + player.name + "[lightgrey].");
-            for(Building b:buildsToKill){
+            for(var iter = Groups.build.iterator(), Building b; iter.hasNext(); b = iter.next();) {
                 if(!(b.block instanceof CoreBlock)){
                     b.tile.setNet(Blocks.air);
                 }
             }
+        });
+        
+        handler.<Player>register("gameover", "Instantly triggers a game over. Cores are not killed.", (args, player) -> {
+            Call.sendMessage(player.name + " has caused a game over.");
+            Events.fire(GameOverEvent(Team.get(0)));
         });
         
         handler.<Player>register("changelog", "Checks the changelog of garbo plugin", (args, player) -> {
@@ -147,7 +132,7 @@ public class GarbagePlugin extends Plugin{
 "[stat]v1.0.0:[]\nPlugin created\nAdded commands:\n/msg <user> <text...>\n/team <team> [player]\n\n" +
 "[stat]v1.0.1[]\nAdded commands:\n/killall [team]\n\n" +
 "[stat]v1.0.2[]\nAdded commands:\n/wipe [team] [cores]\n/changelog\n\n" +
-"[stat]v1.0.3[]\nAdded commands:\n/setteam <team> [player]\nBug fixes:\nFixed /killall and /wipe not removing all buildings/units. /wipe still can not remove walls\nRemoved the ability to wipe team sharded with cores enabled");
+"[stat]v1.0.3[]\nAdded commands:\n/setteam <team> [player]\n/gameover\nBug fixes:\nFixed /killall and /wipe not removing all buildings/units. /wipe still can not remove walls\nRemoved the ability to wipe team sharded with cores enabled");
         });
     }
 }
